ec58a41019071fe9a626a15c46b673ff2a12dfa5
shanyuchen1 <15432465+shanyuchen1@user.noreply.gitee.com>
2025-09-19 20:44:09 +0800

    add init v1

--- docs/sh/ads.sql
+++ docs/sh/ads.sql
@@ -14,20 +14,7 @@ CREATE EXTERNAL TABLE ads_traffic_stats_by_channel
     LOCATION '/warehouse/gmall/ads/ads_traffic_stats_by_channel/';
 
 
-insert overwrite table ads_traffic_stats_by_channel
-select * from ads_traffic_stats_by_channel
-union
-select
-    '20250916' ,dt,
-    recent_days,
-    channel,
-    cast(count(distinct(mid_id)) as bigint) uv_count,
-    cast(avg(during_time_1d)/1000 as bigint) avg_duration_sec,
-    cast(avg(page_count_1d) as bigint) avg_page_count,
-    cast(count(*) as bigint) sv_count,
-    cast(sum(if(page_count_1d=1,1,0))/count(*) as decimal(16,2)) bounce_rate
-from dws_traffic_session_page_view_1d lateral view explode(array(1,7,30)) tmp as recent_days
-group by recent_days,channel;
+
 
 select *
 from ads_traffic_stats_by_channel;
